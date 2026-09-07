5d4ced1d1c82eedc882372eb2370c7232fa3bdb4
DannyUPN <daniel2tanjiro@gmail.com>
2026-09-07 11:39:14 -0500

    Update calculo.py

---
+++ calculo.py
@@ -0,0 +1,9 @@
+#notas
+print("sistemas de notas")
+nota1 = int(input(" ingrese la primera nota: "))
+nota2 = int(input(" ingrese la segunda nota: "))
+nota3 = int(input(" ingrese la tercerra nota: "))
+
+suma = nota1 + nota2 + nota3
+resultado = suma / 3
+print ("el resultado de las notas", resultado)
